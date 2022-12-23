28644ff55805ad1380fd878758ad4b134bc88951
Suresh <113286749+suresh-2603@users.noreply.github.com>
2022-12-23 16:52:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 9000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! - welcome to icu program -change new')
+  res.send('Hello World! - welcome to icu program -change new1')
 })
 
 app.listen(port, () => {
